8da94d75ae0c38fc68b328dcf6ab327b04b9b5e1
Germán Boquizo Sánchez <xekevara@gmail.com>
2025-05-15 02:29:27 +0200

    feat(arcaneDevDeletion): 🚧 changing to actions pattern

---
+++ src/Actions/DeleteLogAction.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boquizo\FilamentLogViewer\Actions;
+
+use Illuminate\Filesystem\Filesystem;
+use RuntimeException;
+
+class DeleteLogAction
+{
+    /**
+     * @throws \Throwable
+     */
+    public static function execute(string $date): true
+    {
+        return (new self())($date);
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function __invoke(string $date): true
+    {
+        $path = ExtractLogPathAction::execute($date);
+
+        $system = new Filesystem();
+
+        throw_unless(
+            $system->delete($path),
+            new RuntimeException('There was an error deleting the log.'),
+        );
+
+        return true;
+    }
+}
---
+++ src/Actions/DownloadLogAction.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boquizo\FilamentLogViewer\Actions;
+
+use Illuminate\Support\Facades\Config;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class DownloadLogAction
+{
+    public static function execute(string $date): BinaryFileResponse
+    {
+        return (new self())($date);
+    }
+
+    public function __invoke(
+        string $date,
+        ?string $filename = null,
+        array $headers = [],
+    ): BinaryFileResponse {
+        if ($filename === null) {
+            $filename = sprintf(
+                "%s{$date}.%s",
+                Config::string('log-viewer.download.prefix', 'laravel-'),
+                Config::string('log-viewer.download.extension', 'log')
+            );
+        }
+
+        $path = ExtractLogPathAction::execute($date);
+
+        return response()->download($path, $filename, $headers);
+    }
+}
