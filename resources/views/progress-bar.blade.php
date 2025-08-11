dc2d647159db66cb29f0e4ec422b6816f8366d77
Germán Boquizo Sánchez <xekevara@gmail.com>
2025-08-11 04:01:52 +0200

    chore: 💄add progress bar

--- resources/views/progress-bar.blade.php
+++ resources/views/progress-bar.blade.php
@@ -1,12 +1,10 @@
-@php
-    $blankSpace = str_repeat(' ', 40);
-@endphp
-<div style="margin-top: 8px;">
-    <div style="align-items: center; font-size: 12px; margin-bottom: 8px;">
-        <span style="width: 100%; font-weight: 700; color: #1f2937;">{{ $percent }}% {!! $blankSpace !!}</span>
+{!! $style !!}
+
+<div class="w-full bg-gray-200 dark:bg-gray-700 h-1 rounded-full overflow-hidden shadow-inner">
+    <div class="h-full rounded-full transition-width duration-400 ease-in-out"
+        style="width: {{ $percent }}%; background-color: {{ $progressColor }};">
     </div>
-    <div style="width: 100%; background-color: #e5e7eb; height: 12px; border-radius: 6px; overflow: hidden; box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1);">
-        <div style="width: {{ $percent }}%; background-color: {{ $progressColor }}; height: 100%; transition: width 0.4s cubic-bezier(0.4, 0, 0.2, 1); border-radius: 6px;">
-        </div>
-    </div>
+</div>
+<div class="items-center mt-4 w-full">
+    <span class="w-full text-gray-800 dark:text-gray-200">{{ $percent }}% </span>
 </div>
