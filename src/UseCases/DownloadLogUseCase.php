d593fff24830e583456b5882277afdd2e244d36b
Germán Boquizo Sánchez <xekevara@gmail.com>
2025-08-26 15:52:13 +0200

    refactor: ♻️ abstract filename use case

--- src/UseCases/DownloadLogUseCase.php
+++ src/UseCases/DownloadLogUseCase.php
@@ -4,35 +4,22 @@ declare(strict_types=1);
 
 namespace Boquizo\FilamentLogViewer\UseCases;
 
-use Boquizo\FilamentLogViewer\FilamentLogViewerPlugin;
-use Illuminate\Support\Facades\Config;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DownloadLogUseCase
 {
-    public static function execute(string $date): BinaryFileResponse
+    public static function execute(string $file): BinaryFileResponse
     {
-        return (new self())($date);
+        return (new self())($file);
     }
 
     public function __invoke(
-        string $date,
+        string $file,
         ?string $filename = null,
         array $headers = [],
     ): BinaryFileResponse {
-        $driver = FilamentLogViewerPlugin::get()->driver();
-
-        $filename = match ($driver) {
-            'stack' => basename($filename ?? ''),
-            'daily' => sprintf(
-                "%s{$date}.%s",
-                Config::string('log-viewer.download.prefix', 'laravel-'),
-                Config::string('log-viewer.download.extension', 'log')
-            ),
-            default => $filename,
-        };
-
-        $path = ExtractLogPathUseCase::execute($date);
+        $filename = ExtractFilenameUseCase::execute($filename, $file);
+        $path = ExtractLogPathUseCase::execute($file);
 
         return response()->download($path, $filename, $headers);
     }
