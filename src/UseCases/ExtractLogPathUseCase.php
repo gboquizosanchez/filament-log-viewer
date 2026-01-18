95fa3ac76acd7b1139dc103341a3141f2696bd07
Germán Boquizo Sánchez <xekevara@gmail.com>
2026-01-18 01:45:03 +0100

    docs: 📝 adding filament 5 support (#50)
    Solves #50

--- src/UseCases/ExtractLogPathUseCase.php
+++ src/UseCases/ExtractLogPathUseCase.php
@@ -34,14 +34,12 @@ class ExtractLogPathUseCase
         $extension = Config::string('filament-log-viewer.pattern.extension', '.log');
         $storagePath = Config::string('filament-log-viewer.storage_path', storage_path('logs'));
 
-        $basePath = $storagePath . DIRECTORY_SEPARATOR;
+        $basePath = $storagePath.DIRECTORY_SEPARATOR;
 
         return match (FilamentLogViewerPlugin::get()->driver()) {
-            'daily' => $basePath . $prefix . $name . $extension,
-            'single' => $basePath . rtrim($prefix, '-') . $extension,
-            'raw' => str_starts_with($name, '/')
-            ? $name
-            : $basePath . $name,
+            'daily' => $basePath.$prefix.$name.$extension,
+            'single' => $basePath.rtrim($prefix, '-').$extension,
+            'raw' => str_starts_with($name, '/') ? $name : $basePath.$name,
         };
     }
 }
